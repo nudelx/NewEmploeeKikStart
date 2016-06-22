7d4cfc8a36e41a1c561632e0f7e6841fd6348572
nudelx <alex.nudelman@samanage.com>
2016-06-22 13:22:22 +0300

    added nav bar

--- src/components/app.js
+++ src/components/app.js
@@ -1,6 +1,8 @@
 import React, { PropTypes } from 'react';
+import Title from './title';
 const App = ({ params }) => (
   <div>
+    <Title title='This is a Title' />
     <div>{`React simple starter # page ${params.page}`}</div>
   </div>);
 App.propTypes = {
